f9638325a9acf2a60edd17d9d85d40fbf35cf483
M H Fahim <mhfahim222@gmail.com>
2026-06-21 09:33:44 +0600

    run.bat: clean output format (hide STAGE logs, show only result)

--- release/run.bat
+++ release/run.bat
@@ -1,2 +1,13 @@
 @echo off
-"%~dp0aurorac.exe" %* --run
+setlocal
+set "TMPFILE=%TEMP%\aurora_err_%RANDOM%.tmp"
+echo.
+echo ---- Output ----
+"%~dp0aurorac.exe" %* --run 2>"%TMPFILE%"
+if %ERRORLEVEL% neq 0 (
+    echo [ERROR]
+    type "%TMPFILE%"
+)
+del "%TMPFILE%" 2>nul
+echo -----------------
+echo.
