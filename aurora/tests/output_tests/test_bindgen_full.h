693795d8c9b6e193f35e053f15ec81a83d21072c
M H Fahim <mhfahim222@gmail.com>
2026-06-27 16:50:55 +0600

    Remove test files, build artifacts, and junk files

--- aurora/tests/output_tests/test_bindgen_full.h
+++
@@ -1,4 +0,0 @@
-int add(int a, int b);
-double sqrt(double x);
-typedef struct { int x; int y; } Point;
-Point make_point(int x, int y);
--- aurora/tests/output_tests/test_crt.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-int main() {
-    void* p = malloc(100);
-    printf("malloc returned %p\n", p);
-    free(p);
-    return 42;
-}
