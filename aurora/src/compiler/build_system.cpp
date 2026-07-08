e6f87d448f4dd11438a4d0a32361a92d36750609
M H Fahim <mhfahim222@gmail.com>
2026-07-08 15:38:28 +0600

    Fix remaining CI errors: gui.cpp duplicate set_callback, build_system.cpp MSVC getHostCPUFeatures

--- aurora/src/compiler/build_system.cpp
+++ aurora/src/compiler/build_system.cpp
@@ -74,12 +74,7 @@ llvm::TargetMachine* create_target_machine(llvm::Module* module, const BuildConf
 
     std::string features_str;
     if (cfg.target_triple.empty()) {
-#if defined(_MSC_VER)
-        llvm::StringMap<bool> host_features;
-        llvm::sys::getHostCPUFeatures(host_features);
-#else
         auto host_features = llvm::sys::getHostCPUFeatures();
-#endif
         for (const auto& f : host_features) {
             if (!features_str.empty()) features_str += ",";
             features_str += f.second ? "+" : "-";
